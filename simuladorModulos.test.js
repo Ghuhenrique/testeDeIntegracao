8d6f72fd0f28819f9525991da390826717e285b4
Bartolomeu Speciorin Gusella <bartolomeu.gusella@detran.df.gov.br>
2026-04-06 10:56:08 -0300

    adciona test suit para simulador de modulos

--- simuladorModulos.test.js
+++ simuladorModulos.test.js
@@ -1,15 +1,17 @@
 // import sistema from "./simuladorModulos.js";
 const sistema = require("./simuladorModulos.js");
 
-console.log(sistema);
+describe('Simulador de Módulos', () => {
 
-// Testes de integração
-test("Fluxo completo com sucesso", () => {
+  // Testes de integração
+  test("Fluxo completo com sucesso", () => {
     const resultado = sistema("admin", "123");
     expect(resultado).toBe("Bem-vindo, Administrador. Perfil: Administrador do sistema");
-});
+  });
 
-test("Fluxo completo com falha", () => {
+  test("Fluxo completo com falha", () => {
     const resultado = sistema("user", "123");
     expect(resultado).toBe("Acesso não autorizado");
+  });
+  
 });
